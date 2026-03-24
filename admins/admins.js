de85ac9776ec43bdaa83ab3d62c1fc59d9d893be
SHERMAN-91 <shermansher402@gmail.com>
2026-03-23 22:42:21 -0600

    updated

--- admins/admins.js
+++ admins/admins.js
@@ -1,12 +1,26 @@
 // import { our_clients } from "../module"
 
 // import { our_clients } from "../module.js";
+window.onload=function(){
+    
+
+
+}
+window.addEventListener("load", function(){
+    if (localStorage.getItem("admin_setting") != null){
+        
+
+this.document.body.classList.remove("d-none")
 
 
 let admin_name = document.getElementById("name_admin")
+
 let admin_info = JSON.parse(localStorage.getItem("admin_setting"))
+
 var theme_link = document.getElementById("theme_color")
+
 admin_name.textContent = admin_info.name
+
 if(admin_info.theme!=null){
     theme_link.setAttribute("href","./themes/"+admin_info.theme+".css")
 }
@@ -16,3 +30,8 @@ else{
 // Theme changeing
 // var theme_link = document.getElementById("theme_color").href="./themes/retro.css"
 // console.log(theme_link)
+    }else{
+ document.location.href="../index.html"
+        }
+
+})
