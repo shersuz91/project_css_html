6e9cf49e848cd2fafe92e07b1ded0266f4d6886d
SHERMAN-91 <shermansher402@gmail.com>
2026-03-28 20:02:57 -0600

    all done

--- home.js
+++ home.js
@@ -7,6 +7,14 @@ var prev = document.getElementById("prev")
 var next = document.getElementById("next")
 var card_box =  document.querySelector(".card_box")
 var cards = document.querySelectorAll(".cards")
+var about_top_a = document.getElementById("about_top_a")
+var offer_top_a = document.getElementById("offer_top_a")
+var for_how_top_a = document.getElementById("for_how_top_a")
+var price_top_a = document.getElementById("price_top_a")
+var review_top_a = document.getElementById("review_top_a")
+var conec_top_a = document.getElementById("conec_top_a")
+var a_link = document.querySelectorAll(".a_link")
+var go_up = document.getElementById("go_up")
 document.addEventListener("click", function(e){
     console.log(e.target.matches("[btn]"))
     if (e.target.matches("[btn]")){
@@ -26,7 +34,37 @@ window.addEventListener("scroll", function(){
     }
     else{
         nav.classList.remove("solid")
+    }
 
+      if (window.scrollY > 100){
+        go_up.style.opacity="1"
+    }
+    else{
+go_up.style.opacity="0"
+    }
+    console.log(window.scrollY)
+    for(let i = 0; i< a_link.length; i++){
+            a_link[i].classList.remove("active_section")
+        }
+    if (1345 >window.scrollY && window.scrollY >= 800){
+        
+        about_top_a.classList.add("active_section")
+    }
+    else if(1745 >window.scrollY && window.scrollY >= 1345){
+        offer_top_a.classList.add("active_section")
+    }
+     else if(2245 >window.scrollY && window.scrollY >= 1745){
+        for_how_top_a.classList.add("active_section")
+    }
+
+      else if(3072 >window.scrollY && window.scrollY >= 2245){
+        price_top_a.classList.add("active_section")
+    }
+      else if(3472 >window.scrollY && window.scrollY >= 3072){
+        review_top_a.classList.add("active_section")
+    }
+      else if(window.scrollY >= 3472){
+        conec_top_a.classList.add("active_section")
     }
 })
 
@@ -39,4 +77,3 @@ next.addEventListener("click", function(e){
 card_box.scrollBy(200, 0)
 
 })
-card_box.scrollTo(300, 0)
